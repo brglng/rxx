0fb4a3a782f0d3f62cb9ce7232bc7c0e81d0d592
Zhaosheng Pan <zhaosheng.pan@ximalaya.com>
2019-04-23 16:45:02 +0800

    Add attributes

--- include/rxx.hpp
+++ include/rxx.hpp
@@ -2,11 +2,15 @@
 #define __RXX_HPP__
 
 #include "rxx/algorithm.hpp"
+#include "rxx/attributes.hpp"
+#include "rxx/fs.hpp"
 #include "rxx/invoke.hpp"
+#include "rxx/io.hpp"
 #include "rxx/option.hpp"
 #include "rxx/result.hpp"
 #include "rxx/slice.hpp"
 #include "rxx/str.hpp"
+#include "rxx/string.hpp"
 #include "rxx/type_traits.hpp"
 #include "rxx/utility.hpp"
 #include "rxx/variant.hpp"
---
+++ include/rxx/attributes.hpp
@@ -0,0 +1,9 @@
+#ifndef __RXX_ATTRIBUTES_HPP__
+#define __RXX_ATTRIBUTES_HPP__
+
+#define RXX_DEPRECATED __attribute__((deprecated))
+#define RXX_NORETURN __attribute__((noreturn))
+#define RXX_NODISCARD __attribute__((warn_unused_result))
+#define RXX_MAYBE_UNUSED __attribute__((unused))
+
+#endif /* end of include guard: __RXX_ATTRIBUTES_HPP__ */
