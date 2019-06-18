8b6c395174151d5f4949cc76234b598f6b1170fe
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2019-06-18 18:48:15 +0800

    Add CMake files

---
+++ cmake/Modules/RxxCPackConfig.cmake
@@ -0,0 +1,27 @@
+set(CPACK_GENERATOR "TGZ")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Rust flavored C++ library")
+set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+set(CPACK_SOURCE_IGNORE_FILES
+  /\\\\.git/
+  \\\\.git.*
+  /build/
+  /cmake-build-.*/
+  /\\\\.idea/
+  /\\\\.ycm_extra_conf\\\\..*
+  /GPATH$
+  /GRTAGS$
+  /GSYMS$
+  /GTAGS$
+  \\\\.swp$
+  \\\\.swo$
+  \\\\.DS_Store$
+  \\\\.ccls
+  \\\\.ccls-cache
+  \\\\.clang-format
+  )
+set(CPACK_SOURCE_GENERATOR "TGZ")
+include(CPack)
