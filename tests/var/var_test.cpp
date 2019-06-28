febf4a621114e71042441b7a98525cce9e404604
Zhaosheng Pan <zhaosheng.pan@sololand.moe>
2019-06-29 00:06:38 +0800

    Remove one test case

--- tests/var/var_test.cpp
+++ tests/var/var_test.cpp
@@ -18,45 +18,7 @@ TEST_CASE("Var type is correct", "[Var]") {
     REQUIRE(v.index() == 2);
 }
 
-class Visitor {
-    size_t m_index;
-
-public:
-    Visitor() : m_index(0) {}
-
-    size_t index() { return m_index; }
-
-    void operator()(int) {
-        m_index = 1;
-    }
-
-    void operator()(float) {
-        m_index = 2;
-    }
-
-    void operator()(void*) {
-        m_index = 3;
-    }
-};
-
-TEST_CASE("Var visit works") {
-    Var<int, float, void*> v;
-    Visitor visitor;
-
-    v = 123;
-    visit(visitor, v);
-    REQUIRE(visitor.index() == v.index() + 1);
-
-    v = 1.0f;
-    visit(visitor, v);
-    REQUIRE(visitor.index() == v.index() + 1);
-
-    v = nullptr;
-    visit(visitor, v);
-    REQUIRE(visitor.index() == v.index() + 1);
-}
-
-TEST_CASE("match works") {
+TEST_CASE("Var visit and match works") {
     Var<int, float, void*> v;
 
     size_t id = 0;
@@ -65,7 +27,7 @@ TEST_CASE("match works") {
     RXX_MATCH(v,
         [&id](int) { id = 1; },
         [&id](float) { id = 2; },
-        [&id](void*) { id = 3; }
+        [&id](void*) { id = 3; },
     );
     REQUIRE(id == v.index() + 1);
 
